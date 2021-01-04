1a466e2042554877b71793e2c2dc536fac594847
Rafiqul islam robin <kratosrobin467@gmail.com>
2021-01-04 22:16:53 +0600

    exam 1

---
+++ exam-1/problem-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please give your input: ");
+            string x = Console.ReadLine();
+            int i;
+            string j = "";
+            for (i = 0; i < x.Length; i++)
+            {
+                if (x[i] == '!' || x[i] == '&' || x[i] == '#' || x[i] == '$' || x[i] == '@')
+                {
+
+                }
+                else
+                {
+                    j = j + x[i];
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("your output without special characters: " + j);
+        }
+
+    }
+}
---
+++ exam-1/problem-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your number");
+            int x = int.Parse(Console.ReadLine());
+            int i;
+            
+            for (i=2; i<x; i++)
+            {
+                if (x % i == 0)
+                {
+                    Console.WriteLine("your number's prime factors : " + i);
+                }
+            }
+        }
+    }
+}

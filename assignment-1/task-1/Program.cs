d39893886be580a333c527dfef8733f2357e52b5
Rafiqul islam robin <kratosrobin467@gmail.com>
2021-01-23 23:57:58 +0600

    changed

--- assignment-1/task-1/Program.cs
+++ assignment-1/task-1/Program.cs
@@ -1,81 +1,98 @@
-﻿/*
- * Task 1) Write code to subtract two big numbers. Numbers can be 100 digit long. First number 
-  can be smaller.
- */
-using System;
+﻿using System;
+using System.Collections;
 
-
-namespace task_1
+class GFG
 {
-    class Program
+
+    
+    static bool Smaller(string str1, string str2)
     {
-        static void Main(string[] args)
+      
+        int n1 = str1.Length, n2 = str2.Length;
+        if (n1 < n2)
+            return true;
+        if (n2 < n1)
+            return false;
+
+        for (int i = 0; i < n1; i++)
+            if (str1[i] < str2[i])
+                return true;
+            else if (str1[i] > str2[i])
+                return false;
+
+        return false;
+    }
+   static string Difference(string str1, string str2)
+    {
+       
+        if (Smaller(str1, str2))
         {
-            while (true)
-            {
-                var number1 = Console.ReadLine();
-                var number2 = Console.ReadLine();
-            
-                if (string.IsNullOrEmpty(number1) || string.IsNullOrEmpty(number2))
-                    break;
-
-                if (number1.Length < number2.Length)
-                {
-                    var temp = number1;
-                    number1 = number2;
-                    number2 = temp;
-                }
-                var result = new int[number1.Length];
-
-                var i = number1.Length - 1;
-                var j = number2.Length - 1;
-                
-                var carry = 0;
-                while (i >= 0)
-                {
-                    var sub = 0;
-                    if (j>=0)
-                    {
-                        sub = (number1[i] - '0') - (number2[j] - '0') - carry;
-                    }
-                    else
-
-                        sub = (number1[i] - '0') - result[i];
-
-                    if (i == 0)
-                    {
-                        result[i] = sub;
-                    }
-                    else
-                    {
-                        if (sub < 0)
-                        {
-                            sub = sub + 10;
-                            carry = 1;
-                        }
-
-                        else
-                            carry = 0;
-                    }
-
-                    result[i] = sub;
-
-
-                    i--;
-                    j--;
-
-                }
-                
-
-                for (var x = 0; x < result.Length; x++)
-                    Console.Write(result[x]);
-
-              
-
-
-            }
-
-            }
+            string t = str1;
+            str1 = str2;
+            str2 = t;
         }
+
+       
+        string result = "";
+
+        
+        int n1 = str1.Length, n2 = str2.Length;
+
+       
+        char[] char1 = str1.ToCharArray();
+        Array.Reverse(char1);
+        str1 = new string(char1);
+        char[] char2 = str2.ToCharArray();
+        Array.Reverse(char2);
+        str2 = new string(char2);
+
+        int carry = 0;
+
+        for (int i = 0; i < n2; i++)
+        {
+      
+            int sub = ((int)(str1[i] - '0')
+                       - (int)(str2[i] - '0') - carry);
+
+          
+            if (sub < 0)
+            {
+                sub = sub + 10;
+                carry = 1;
+            }
+            else
+                carry = 0;
+
+            result += (char)(sub + '0');
+        }
+
+        for (int i = n2; i < n1; i++)
+        {
+            int sub = ((int)(str1[i] - '0') - carry);
+
+            if (sub < 0)
+            {
+                sub = sub + 10;
+                carry = 1;
+            }
+            else
+                carry = 0;
+
+            result += (char)(sub + '0');
+        }
+
+        char[] char3 = result.ToCharArray();
+        Array.Reverse(char3);
+        return new string(char3);
     }
 
+    public static void Main()
+    {
+
+
+        string number1 = Console.ReadLine();
+        string number2 = Console.ReadLine();
+
+        Console.WriteLine(Difference(number1,number2));
+    }
+}
